e6011d5a108b77a450e884ba1d9a500c92dca3fd
Pablo Baenas <contacto@pablobaenas.com>
2020-07-05 16:17:08 +0200

    Showing only enabled pickup points in the frontend

--- Pablobaenas/PickupSystem/Model/Pickups.php
+++ Pablobaenas/PickupSystem/Model/Pickups.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace Pablobaenas\PickupSystem\Model;
 
 class Pickups extends \Magento\Framework\Model\AbstractModel implements \Magento\Framework\DataObject\IdentityInterface
 {
     const CACHE_TAG = 'pablobaenas_pickupsystem_pickup';
-
     protected $_cacheTag = 'pablobaenas_pickupsystem_pickup';
-
     protected $_eventPrefix = 'pablobaenas_pickupsystem_pickup';
+
+    const STATUS_ENABLED = 1;
+    const STATUS_DISABLED = 2;
 
     protected function _construct()
     {
@@ -26,9 +28,18 @@ class Pickups extends \Magento\Framework\Model\AbstractModel implements \Magento
         return $values;
     }
 
+    public function isEnabled()
+    {
+        if ($this->getStatus() == self::STATUS_ENABLED) {
+            return true;
+        }
+
+        return false;
+    }
+
     public function getGoogleMapsURL()
     {
-        $google_maps_url ='https://maps.google.com/?q=';
+        $google_maps_url = 'https://maps.google.com/?q=';
 
         return $google_maps_url . $this->getLatitude() . ',' . $this->getLongitude();
     }
